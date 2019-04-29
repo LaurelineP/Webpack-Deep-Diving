414a14ba24912714e11c248c376651584d5423a9
Laureline <laurelinedev@gmail.com>
2019-04-29 13:30:22 +0200

    upd

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const app = express();
 const port = 3000;
 const url = 'http://localhost:';
 
-app.use(express.static('dist'));
+app.use( express.static('dist') );
 
-app.get('/', (req, res) => {
-    res.sendFile( __dirname + '/index.html' );
-})
+/* Using webpack: there is no use for the bloc bellow */
+// app.get('/', (req, res) => {
+//     res.sendFile( __dirname + '/index.html' );
+// })
 
-app.listen( port, (err, data) => console.log(`Silver's listenning on port: ${url}${port}`));
+app.listen( port, ( err, data ) => console.log(`Silver's listenning on port: ${url}${port}`));
